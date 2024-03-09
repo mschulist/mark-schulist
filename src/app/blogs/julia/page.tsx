cc7f8bb9021fa0f3cb89c0e55c803ead56e39e72
Mark Schulist <mschulist2@gmail.com>
2024-03-08 19:01:48 -0600

    made julia blog post

---
+++ src/app/blogs/julia/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from "react";
+import IframeResizer from "iframe-resizer-react";
+
+export default function JuliaBlog() {
+    return (
+        <div>
+            <IframeResizer 
+                src="/julia.html" 
+                style={{width: '1px', minWidth: '100%'}}
+                checkOrigin={false}
+            />
+        </div>
+    )
+}
--- src/components/BlogLanding.tsx
+++ src/components/BlogLanding.tsx
@@ -3,9 +3,6 @@ import { cn } from "@/utils/cn";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
 import Image from "next/image";
 
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
-);
 
 const blogPosts = [
     {
@@ -21,12 +18,19 @@ const blogPosts = [
       className: "md:col-span-1 bg-gradient-to-r from-zinc-700 to-zinc-800",
       header: <Image src={"/typst_comp.gif"} alt="" height={0} width={800} className="h-4/6 rounded-lg"/>,
       href: "/blogs/typst",
+    },
+    {
+      title: "Exploring Julia",
+      description: "A better way to do data science?",
+      className: "md:col-span-1 bg-gradient-to-r from-zinc-700 to-zinc-800",
+      header: <Image src={"https://julialang.org/assets/infra/logo.svg"} alt="" height={0} width={400} className="h-4/6 rounded-lg"/>,
+      href: "/blogs/julia",
     }
 ];
 
 const BlogLanding: React.FC = () => {
   return (
-    <BentoGrid className="grid-cols-1 md:grid-cols-2 gap-4">
+    <BentoGrid className="grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-20 items-baseline p-10">
       {blogPosts.map((post, i) => (
         <a href={post.href} key={i}>
         <BentoGridItem
