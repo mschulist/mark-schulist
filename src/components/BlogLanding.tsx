97b185d75c1aad6ae3f2418a557954d3d1d1b7b4
Mark Schulist <mschulist2@gmail.com>
2024-03-13 13:36:51 -0700

    added gradient

--- src/components/BlogLanding.tsx
+++ src/components/BlogLanding.tsx
@@ -8,38 +8,38 @@ const blogPosts = [
     {
         title: "The Metropolis Algorithm",
         description: "Starting to understand MCMC",
-        className: "md:col-span-2 bg-gradient-to-r from-cyan-950 to-zinc-800",
-        header: <Image src={"/metropolis_clip.gif"} alt="" height={0} width={400} className="h-4/6 rounded-lg"/>,
+        className: "md:col-span-1 bg-gradient-to-r from-cyan-950 to-zinc-800 h-full justify-end",
+        header: <Image src={"/metropolis_clip.gif"} alt="" height={0} width={400} className="rounded-lg"/>,
         href: "/blogs/metropolis",
     },
     {
       title: "Typst",
       description: "Why I love Typst",
-      className: "md:col-span-1 bg-gradient-to-r from-cyan-950 to-zinc-800",
-      header: <Image src={"/typst_comp.gif"} alt="" height={0} width={800} className="h-4/6 rounded-lg"/>,
+      className: "md:col-span-1 bg-gradient-to-r from-cyan-950 to-zinc-800 h-full justify-end",
+      header: <Image src={"/typst_comp.gif"} alt="" height={0} width={800} className="rounded-lg"/>,
       href: "/blogs/typst",
     },
     {
       title: "Exploring Julia",
       description: "A better way to do data science?",
-      className: "md:col-span-1 bg-gradient-to-r from-cyan-950 to-stone-800",
-      header: <Image src={"https://julialang.org/assets/infra/logo.svg"} alt="" height={0} width={400} className="h-4/6 rounded-lg"/>,
+      className: "md:col-span-1 bg-gradient-to-r from-cyan-950 to-stone-800 h-full justify-end",
+      header: <Image src={"https://julialang.org/assets/infra/logo.svg"} alt="" height={0} width={350} className="rounded-lg"/>,
       href: "/blogs/julia",
     },
     {
       title: "Understanding the Gradient",
       description: "An exploration of minimizing function using gradient descent",
-      className: "md:col-span-1 bg-gradient-to-r from-cyan-950 to-zinc-800",
-      header: <Image src="/gradient.gif" alt="" width={444.5} height={0} className="rounded-lg"/>,
+      className: "md:col-span-1 bg-gradient-to-r from-cyan-950 to-zinc-800 h-full justify-end",
+      header: <Image src="/gradient.gif" alt="" width={400} height={0} className="rounded-lg"/>,
       href: "blogs/gradient"
     }
 ];
 
 const BlogLanding: React.FC = () => {
   return (
-    <BentoGrid className="grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-20 items-baseline p-10">
+    <BentoGrid className="grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-20 items-center p-10">
       {blogPosts.map((post, i) => (
-        <a href={post.href} key={i}>
+        <a href={post.href} key={i} className="h-full">
         <BentoGridItem
           key={i}
           className={post.className}
